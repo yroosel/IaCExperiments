c8725cb5c0aac4a513f8a110172023b2489b4da4
yroosel <yvan.rooseleer@biasc.be>
2021-12-16 12:14:58 +0100

    Updated process device versions shell script

--- scripts/process_device_versions.sh
+++ scripts/process_device_versions.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-# Before running this script it is necessary to create a subfolder with show version output files
-# Task: this script will use output from "show version"
+# Before running this script it is necessary to create a subfolder with running config files
+# Task: this script to use output from  "show version"
 echo "START DEVICE IOS VERSION  CHECK"
 REQUIRED_IOS="16.09.05"
 VERSION_SEARCH_TEXT="Cisco IOS XE Software, Version"
 echo REQUIRED IOS: $REQUIRED_IOS
-
 echo $(date +"%F")
 echo $(date +"%T")
 echo 
 for f in device_versions/* 
 do 
   echo "BEGIN -- Processing file $f" 
-  cat $f | grep uptime  | cut -d' ' -f1 #hostname 
+  hostname=$(cat $f | grep uptime  | cut -d' ' -f1)
+  echo Hostname: $hostname
+  model=$(cat $f | grep "K/"  | cut -d' ' -f2) 
+  echo Model: $model
   IOS_VERSION=$(cat $f | grep "$VERSION_SEARCH_TEXT" | cut -d' ' -f6)
-  echo Current IOS Version: $IOS_VERSION
-  if [ $REQUIRED_IOS != $IOS_VERSION ] 
+  echo Current IOS Version: "$IOS_VERSION"
+  if [ "$REQUIRED_IOS" != "$IOS_VERSION" ] 
   then
     echo TO DO: Upgrade IOS version to: $REQUIRED_IOS
   else 
