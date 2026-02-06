ba3a01d126ef51998bcb9ab72adae8fc18943e36
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-02-06 13:59:38 +0100

    Update curl_logging_monitor_put.sh

--- Restconf/curl_logging_monitor_put.sh
+++ Restconf/curl_logging_monitor_put.sh
@@ -1,6 +1,8 @@
-DEVICE_IP=Get IP Address
-curl -i -k -X "PUT" "https://$DEVICE_IP:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity" \
--H 'Content-Type: application/yang-data+json' \
--H 'Accept: application/yang-data+json' \
--u 'get_username:get_password' \
--d $'{"severity": "informational"}'
+IP_ADDRESS="192.168.56.xxx"
+RESTCONF_USERNAME="Your username" 
+RESTCONF_PASSWORD="YOur Password" 
+curl -ik -X PUT \  "https://$IP_ADDRESS:443/restconf/data/Cisco-IOS-XE-native:native/logging" \
+  -H "Content-Type: application/yang-data+json" \
+  -H "Accept: application/yang-data+json" \
+  -u "$RESTCONF_USERNAME:$RESTCONF_PASSWORD" \
+  -d '{"Cisco-IOS-XE-native:logging":{"monitor":{"severity":"informational"}}}'
