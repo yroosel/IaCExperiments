338bdb6b4f4a4f55e9566f3da0ec0b9e413fafab
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-02-07 10:17:12 +0100

    Update curl_logging_monitor_put.sh

--- Restconf/curl_logging_monitor_put.sh
+++ Restconf/curl_logging_monitor_put.sh
@@ -1,6 +1,6 @@
 IP_ADDRESS="192.168.56.xxx"
 RESTCONF_USERNAME="Your username" 
-RESTCONF_PASSWORD="YOur Password" 
+RESTCONF_PASSWORD=YoOur Password" 
 curl -ik -X PUT \  "https://$IP_ADDRESS:443/restconf/data/Cisco-IOS-XE-native:native/logging" \
   -H "Content-Type: application/yang-data+json" \
   -H "Accept: application/yang-data+json" \
